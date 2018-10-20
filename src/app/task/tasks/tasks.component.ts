a354f75d3cb64a834753392ca28b0f6dc3aaf86d
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2018-10-20 18:08:25 +0200

    feat: randomize background color

--- src/app/task/tasks/tasks.component.ts
+++ src/app/task/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { TasksService } from './../shared/tasks.service';
@@ -11,13 +11,23 @@ import { TasksService } from './../shared/tasks.service';
 export class TasksComponent {
   readonly faAngleDoubleRight = faAngleDoubleRight;
   readonly allTasksDone$: Observable<boolean>;
+  @HostBinding('class')
+  colorClass = '';
 
   constructor(private readonly tasksService: TasksService) {
     this.allTasksDone$ = tasksService.allRandomTasksDone$;
+    this.setRandomBackgroundColor();
   }
 
   nextTask() {
     this.tasksService.navigateToRandomTask();
+    this.setRandomBackgroundColor();
+  }
+
+  private setRandomBackgroundColor() {
+    const colorClasses = ['tasks--red', 'tasks--green', 'tasks--yellow', 'tasks--blue', 'tasks--orange', 'tasks--pink'];
+    const randomIndex = Math.floor(Math.random() * colorClasses.length);
+    this.colorClass = colorClasses[randomIndex];
   }
 
   restart() {
