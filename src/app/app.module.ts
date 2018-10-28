2fc51a47473612f97541324857beca9fcaaccf72
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2018-10-28 17:04:53 +0100

    feat: add pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { TaskModule } from './task/task.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, StartComponent],
-  imports: [BrowserModule, TaskModule, AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    TaskModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
